66fc7dc85ddcfb0d2294592453929b0392455985
David Knupp <dknupp@cloudera.com>
2018-02-23 15:46:44 -0800

    Add manifest and license files

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Licensed to the Apache Software Foundation (ASF) under one
+# # or more contributor license agreements.  See the NOTICE file
+# # distributed with this work for additional information
+# # regarding copyright ownership.  The ASF licenses this file
+# # to you under the Apache License, Version 2.0 (the
+# # "License"); you may not use this file except in compliance
+# # with the License.  You may obtain a copy of the License at
+# #
+# #   http://www.apache.org/licenses/LICENSE-2.0
+# #
+# # Unless required by applicable law or agreed to in writing,
+# # software distributed under the License is distributed on an
+# # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# # KIND, either express or implied.  See the License for the
+# # specific language governing permissions and limitations
+# # under the License.
+
 from __future__ import print_function, division
 
 import pip
@@ -34,7 +51,7 @@ def requirements():
 
 
 def get_version():
-    return '2.10.0'
+    return '2.10.0a2'
 
 
 setup(
